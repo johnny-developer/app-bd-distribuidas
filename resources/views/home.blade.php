1b708f5a95159f865fcde453fa6b267e1bb06f1f
erikujum <erik.vasquez@aulavirtual.umar.mx>
2024-01-11 16:14:51 -0600

    estilos avance
    erik

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Base de Datos</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+<link rel="stylesheet" href="{{ asset('css/styleshome.css') }}">
+
+<form action="{{ route('consultar.clientes') }}" method="post">
+    <h1>clientes</h1>
+    @csrf
+    <input type="checkbox" name="opciones1[]" value="ids"> Consultar los ids de los clientes<br>
+    <input type="checkbox" name="opciones1[]" value="nombre"> Consultar los nombres de los clientes<br>
+    <input type="checkbox" name="opciones1[]" value="numero"> Consultar los numeros de los clientes <br>
+    <button type="submit">Consultar</button>
+</form>
+
+<form action="{{ route('consultar.ventas') }}" method="post">
+    <h1>Ventas</h1>
+    @csrf
+    <input type="checkbox" name="opciones2[]" value="ids"> Consultar los ids de las ventas<br>
+    <input type="checkbox" name="opciones2[]" value="fecha"> Consultar las fechas de las ventas<br>
+    <input type="checkbox" name="opciones2[]" value="id_cliente"> Consultar los ids de los cliente de las ventas<br>
+    <input type="checkbox" name="opciones2[]" value="id_producto"> Consultar los ids de los productos de las ventas<br>
+    <button type="submit">Consultar</button>
+</form>
