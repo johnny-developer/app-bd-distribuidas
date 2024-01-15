1aac9a2e603ac3c5a19c5b998b39a314d74cd881
erikujum <erik.vasquez@aulavirtual.umar.mx>
2024-01-15 09:21:04 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,8 @@
 
-<link rel="stylesheet" href="{{ asset('css/styleshome.css') }}">
+<link rel="stylesheet" href="../css/styleshome.css">
 
 <form action="{{ route('consultar.clientes') }}" method="post">
     <h1>clientes</h1>
-    @csrf
     <input type="checkbox" name="opciones1[]" value="ids"> Consultar los ids de los clientes<br>
     <input type="checkbox" name="opciones1[]" value="nombre"> Consultar los nombres de los clientes<br>
     <input type="checkbox" name="opciones1[]" value="numero"> Consultar los numeros de los clientes <br>
@@ -12,7 +11,6 @@
 
 <form action="{{ route('consultar.ventas') }}" method="post">
     <h1>Ventas</h1>
-    @csrf
     <input type="checkbox" name="opciones2[]" value="ids"> Consultar los ids de las ventas<br>
     <input type="checkbox" name="opciones2[]" value="fecha"> Consultar las fechas de las ventas<br>
     <input type="checkbox" name="opciones2[]" value="id_cliente"> Consultar los ids de los cliente de las ventas<br>
